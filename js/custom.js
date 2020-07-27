3ed705f450cec9220c636d787939f1a6b5562184
unknown <kasra.parhun@gmail.com>
2020-07-27 18:34:16 +0430

    Project ended

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,18 @@
-window.addEventListener('keydown', function(evt){
+const keys = document.querySelectorAll("kbd");
+
+function playSound(evt){
   const audio = document.querySelector(`audio[data-code='${evt.keyCode}']`);
   if (!audio) return;
+  const key = document.querySelector(`kbd[data-code="${evt.keyCode}"]`);
+  key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
+}
 
-})
+function removeTransition(evt){
+  if (evt.propertyName !== 'transform') return; // skip if it's not a transform
+  this.classList.remove("playing");
+}
+
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
